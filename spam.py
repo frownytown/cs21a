39ba3a5117ab15e79f9a284c43082327bea0a6cf
Roger <rogerhmcclain@gmail.com>
2017-02-03 21:50:26 -0800

    Update to Spam.py for class

--- spam.py
+++ spam.py
@@ -7,9 +7,11 @@
 # Date: 2/3/2017
 # -----------------------------------------------------------------------------
 """
-Enter your module docstring with a one-line overview here
+This code compares the content of a message with a list of known spam words.
 
-and a more detailed description here.
+Using the list of known spam words this code compares the content of a message
+input by the user to determine the ratio of content that is common spam words.
+if the ratio is above 0.10 the code concludes that the message is spam.
 """
 import string
 SPAM_WORDS = {'opportunity', 'inheritance', 'money', 'rich', 'dictator',
@@ -22,48 +24,51 @@ SPAM_THRESHOLD = 0.10
 
 def spam_indicator(text):
     """
-    Enter your function docstring here
+    This function transforms input into a list and compares it to spam word list
+    and returns the ratio of spam words to non spam words.
     """
     # This function returns the spam indicator rounded to two decimals
 
-    word_list = text.split()
-    unique_words = set(word_list)
-    shared_words = unique_words & SPAM_WORDS
-    shared_ratio = (len(shared_words)) / (len(unique_words))
-    rounded_ratio = round(shared_ratio, 2)
-    return rounded_ratio
+    word_list = text.split()        # Turning string into list
+    unique_words = set(word_list)   # Turning list into set
+    shared_words = unique_words & SPAM_WORDS    # Intersection of two sets
+    shared_ratio = (len(shared_words)) / (len(unique_words))   # Finding ratio
+    rounded_ratio = round(shared_ratio, 2)      # Rounding ratio to two places
+    return rounded_ratio        # Return rounded ratio
 def classify(indicator):
     """
-    Enter your function docstring here
+    This function prints whether or not the message is spam.
     """
     # This function prints the spam classification
-    if indicator > SPAM_THRESHOLD:
+    if indicator > SPAM_THRESHOLD:  # If ratio above 0.10 then SPAM
         print('Spam indicator: ' + str(indicator))
         print('This message is: SPAM')
-    else:
+    else:                           # If ratio anything else then HAM
         print('Spam indicator: ' + str(indicator))
         print('This message is: HAM')
 
 def get_input():
     """
-    Enter your function docstring here
+    This is the funtion that prompts the user for input, it also excludes
+    punctuation and converts everything to lowercase.
     """
     # Prompt the user for input and return the input
     while True:
-        user_input = input('Please enter your message: ')
-        lower_input = user_input.lower()
+        user_input = input('Please enter your message: ')   # Prompt for input
+        lower_input = user_input.lower()             # Convert to lowercase
+        # Creates a set of punctuation in order to exclude them from our code
         exclude_chars = set(string.punctuation)
         fixed_input = ''.join(ch for ch in lower_input if ch not in
-                              exclude_chars)    # remove punctuation
-        return fixed_input
+                              exclude_chars)    # Remove punctuation
+        return fixed_input      # Return input that is cleaned up
 def main():
     # Get the user input and save it in a variable
     # Call spam_indicator to compute the spam indicator and save it
     # Print the spam_indicator
     # Call classify to print the classification
-    lower_input = get_input()
-    final_ratio = spam_indicator(lower_input)
-    classify(final_ratio)
+    lower_input = get_input()   # Var lower_input is what get_input() returns
+    final_ratio = spam_indicator(lower_input)   # Pass lower_input into spam
+    classify(final_ratio)   # Pass the rounded ratio into classify and print
 
 if __name__ == '__main__':
-    main()
+    main()      # Call main function
